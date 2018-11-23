33d77567ddf2e74bcf759bbc1dc4fbf86cf9a4bf
Jędrzej Bryll <jedrzej.bryll@gmail.com>
2018-11-23 05:01:41 +0100

    basic api client setup

--- lib/api/client.rb
+++ lib/api/client.rb
@@ -5,6 +5,27 @@ module Lunching
   module Api
     class Client
       include Endpoints
+
+      API_DOMAIN = 'https://api.lunching.pl/api'.freeze
+      LOGIN_EMAIL = ''.freeze
+      PASSWORD = ''.freeze
+
+      def initialize
+        @client = HTTP.basic_auth(user: LOGIN_EMAIL, pass: PASSWORD)
+      end
+
+      Endpoints.public_instance_methods.each do |endpoint_name|
+        define_method(endpoint_name) do |*args, **kwargs|
+          JSON.parse super(*args, *kwargs).body
+        end
+      end
+
+      [:get, :post].each do |request_method|
+        define_method(request_method) do |path, **kwargs|
+          @client.public_send(request_method, "#{API_DOMAIN}#{path}", *kwargs)
+        end
+        private request_method
+      end
     end
   end
 end
--- lib/api/endpoints.rb
+++ lib/api/endpoints.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 module Lunching
   module Api
     module Endpoints
+
+      def restaurants
+        get('/restaurants')
+      end
+
+      def restaurant_categories(restaurant_id)
+        get("/restaurants/#{restaurant_id}/categories")
+      end
+
     end
   end
 end
