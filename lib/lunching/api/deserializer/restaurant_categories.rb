5c62b70907388e162965e21621306531885aa751
Jędrzej Bryll <jedrzej.bryll@gmail.com>
2018-12-07 09:16:41 +0100

    minor tweaks

--- lib/lunching/api/deserializer/restaurant_categories.rb
+++ lib/lunching/api/deserializer/restaurant_categories.rb
@@ -3,22 +3,26 @@ module Lunching
     module Deserializer
       class RestaurantCategories < Base
 
+        ITEM_SIZE_MAPPER = proc do |size|
+          Model::ItemSize.new(
+            lunching_id: size['itemFeatureId'],
+            price: Float(size['price'])
+          )
+        end
+
+        CATEGORY_ITEM_MAPPER = proc do |item|
+          Model::CategoryItem.new(
+            name: "#{item['name']} #{item['description']}",
+            sizes: item['sizes'].map(&ITEM_SIZE_MAPPER)
+          )
+        end
+
         def call
           super.map do |restaurant_category|
             Model::RestaurantCategory.new(
               lunching_id: restaurant_category['categoryId'],
               name: restaurant_category['name'],
-              items: restaurant_category['items'].map do |item|
-                Model::CategoryItem.new(
-                  name: item['name'] + ' ' + item['description'],
-                  sizes: item['sizes'].map do |item_size|
-                    Model::ItemSize.new(
-                      lunching_id: item_size['itemFeatureId'],
-                      price: item_size['price'].to_s + 'zł'
-                    )
-                  end
-                )
-              end
+              items: restaurant_category['items'].map(&CATEGORY_ITEM_MAPPER)
             )
           end
         end
